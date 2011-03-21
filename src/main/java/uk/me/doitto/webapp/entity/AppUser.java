a8319a66547c8c7c7cee8f1b6d4b4037488e8485
Ian Smith <m4r35n357@gmail.com>
2011-03-21 15:15:48 +0000

    forgot to add jaxb context init to appuser class

--- src/main/java/uk/me/doitto/webapp/entity/AppUser.java
+++ src/main/java/uk/me/doitto/webapp/entity/AppUser.java
@@ -26,7 +26,6 @@ import javax.persistence.Entity;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
 import javax.xml.bind.annotation.XmlRootElement;
 
 import uk.me.doitto.webapp.dao.AbstractEntity;
@@ -44,14 +43,7 @@ public class AppUser extends AbstractEntity {
 
     private static final long serialVersionUID = 1L;
 
-    private static final JAXBContext jaxbContext;
-    static {
-    	try {
-    		jaxbContext = JAXBContext.newInstance(AppUser.class);
-		} catch (JAXBException e) {
-			throw new RuntimeException(e);
-		}
-    }
+    private static final JAXBContext jaxbContext = initJaxbContext(AppUser.class);
 
 	private String password;
 
